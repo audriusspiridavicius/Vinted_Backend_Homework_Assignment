425a0ff5517cb4389df58c30367193c682c4db79
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-04 01:32:00 +0300

    basic delivery price calculation class

---
+++ delivery.py
@@ -0,0 +1,29 @@
+from delivery_rule import DeliveryRule
+from member import Member
+from transaction import Transaction, MemberTransaction
+
+class Delivery:
+    
+    _delivery_price = 0
+    _discount = 0
+    
+    def __init__(self, delivery_rules:list[DeliveryRule]) -> None:
+        self.delivery_rules = delivery_rules
+    
+    
+    def calculate(self,transaction:Transaction, member:Member):
+        self._calculate(transaction, member)
+        member.add_transaction(transaction=MemberTransaction(**transaction.__dict__, price=self._delivery_price)) # ????????
+            
+    def _calculate(self,transaction:Transaction, member:Member):
+                
+        for rule in self.delivery_rules:
+            # if rule.provider == transaction.provider and rule.size == transaction.package_size:
+            if rule.check_rule(transaction, member):
+                self._delivery_price = rule.price
+                break
+        else:
+
+            self._delivery_price = "Ignored"
+
+
