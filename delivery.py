820df5030a30d51152591c3734b23adc9e8c470b
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-05 15:32:17 +0300

    delivery calculation logic update

--- delivery.py
+++ delivery.py
@@ -1,6 +1,7 @@
+from delivery_data import DeliveryData
 from delivery_rule import DeliveryRule
 from member import Member
-from transaction import Transaction, MemberTransaction
+from transaction import Transaction
 
 class Delivery:
     
@@ -11,9 +12,17 @@ class Delivery:
         self.delivery_rules = delivery_rules
     
     
-    def calculate(self,transaction:Transaction, member:Member):
+    def calculate(self,transaction:Transaction, member:Member) -> DeliveryData:
         self._calculate(transaction, member)
-        member.add_transaction(transaction=MemberTransaction(**transaction.__dict__, price=self._delivery_price)) # ????????
+        
+        # delivery discount manager functionality here ??
+        
+        delivery_data = DeliveryData()
+        delivery_data.delivery_price = self._delivery_price
+        # member.add_transaction(transaction=MemberTransaction(**transaction.__dict__, price=self._delivery_price))
+        
+        return delivery_data
+        
             
     def _calculate(self,transaction:Transaction, member:Member):
                 
@@ -29,8 +38,8 @@ class Delivery:
 
 class SSizePackageDelivery(Delivery):
     
-    def _calculate(self, transaction: Transaction, member: Member):
-        print("SSizePackageDelivery")
+    
+    def calculate(self, transaction: Transaction, member: Member):
         min_delivery_price = float("inf")
         
         for rule in self.delivery_rules:
@@ -42,3 +51,7 @@ class SSizePackageDelivery(Delivery):
                 
         self._discount = self._delivery_price - min_delivery_price
         self._delivery_price = self._delivery_price - self._discount
+        
+        
+        return DeliveryData(delivery_price=self._delivery_price, discount=self._discount)
+       
