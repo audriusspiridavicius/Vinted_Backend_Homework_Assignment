8341483a36296b4bd3bab98b83c67bf3e9e1a101
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-05 15:33:47 +0300

    Transaction class refactored

--- transaction.py
+++ transaction.py
@@ -1,19 +1,32 @@
+from enums import PackageSizeEnum, DeliveryProviderEnum
 class Transaction:
-    def __init__(self, date, package_size, provider):
+    def __init__(self, date, package_size:str, provider:str):
         
         self.date = date
-        self.package_size = package_size
-        self.provider = provider
+        self._package_size = package_size
+        self._provider = provider
+    
+    @property
+    def package_size(self):
+        return PackageSizeEnum.__members__.get(self._package_size, self._package_size) 
+    
+    @property
+    def provider(self):
+        return DeliveryProviderEnum.__members__.get(self._provider, self._provider) 
+    
     
     def __str__(self) -> str:
-        return f"{self.date} {self.package_size} {self.provider}"
+        return f"{self.date} {self.package_size.name if isinstance(self.package_size, PackageSizeEnum) else self.package_size}{f' {self.provider.name}' if isinstance(self.provider, DeliveryProviderEnum) else self.provider}"
     
     def __repr__(self) -> str:
         return self.__str__()
 
     
 class MemberTransaction(Transaction):
-    def __init__(self, date, package_size, provider, price = 0, discount = 0):
-        super().__init__(self, date, package_size, provider)
+    def __init__(self, date, _package_size:str, _provider:str, price = 0, discount = 0):
+        super().__init__(date, _package_size, _provider)
         self.price = price
         self.discount = discount
+    
+    def __str__(self) -> str:
+        return f"{super().__str__()} {self.price} {self.discount if self.discount > 0 else ("" if self.price == "Ignored" else '-')}"
