6886f079292f79488e5b58009ddf74bb40ec35d5
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-05 15:31:25 +0300

    delivery data class

---
+++ delivery_data.py
@@ -0,0 +1,7 @@
+class DeliveryData:
+    
+    
+    def __init__(self, delivery_price:float = 0, discount:float = 0) -> None:
+        self.delivery_price = delivery_price
+        self.discount = discount
+    
