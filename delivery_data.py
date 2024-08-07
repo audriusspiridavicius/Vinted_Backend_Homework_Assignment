7451542013938bcea9bddd285fbefd90d859083f
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-07 22:46:41 +0300

    DeliveryData discout, delivery price set/get methods added

--- delivery_data.py
+++ delivery_data.py
@@ -1,6 +1,27 @@
 class DeliveryData:
     
     
+    
+    @property
+    def discount(self):
+        return self._discount
+    
+    @property
+    def delivery_price(self):
+        return self._delivery_price
+    
+    
+    @discount.setter
+    def discount(self, value):
+        self._discount = round(value, 2)
+        
+    @delivery_price.setter
+    def delivery_price(self, value):
+        if isinstance(value, int) or isinstance(value, float):
+            self._delivery_price = round(value, 2)
+        else:
+            self._delivery_price = value
+            
     def __init__(self, delivery_price:float = 0, discount:float = 0) -> None:
         self.delivery_price = delivery_price
         self.discount = discount
