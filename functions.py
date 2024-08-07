eab43a4baaef7f2e6a1a05d8a322721f0206db7e
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-07 20:26:36 +0300

    get_days_in_month function

--- functions.py
+++ functions.py
@@ -3,3 +3,15 @@ def is_leap_year(year):
         return True
     return False
 
+def get_days_in_month(year, month):
+    if month in [1, 3, 5, 7, 8, 10, 12]:  
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    elif month == 2:
+        if is_leap_year(year):
+            return 29
+        else:
+            return 28
+    else:
+        raise ValueError("Invalid month. Month should be between 1 and 12.")
