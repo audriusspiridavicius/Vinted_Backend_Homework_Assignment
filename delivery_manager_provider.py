5e64074300fc5d91e5b1aa31ad3a7b92e7903bfa
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-07 20:34:19 +0300

    FreeDeliveryNTimesMonth added to DeliveryManagerProvider

--- delivery_manager_provider.py
+++ delivery_manager_provider.py
@@ -1,7 +1,7 @@
 from transaction import Transaction
 from member import Member
-from enums import PackageSizeEnum
-from delivery import SmallestDeliveryPriceAmongProviders, Delivery
+from enums import PackageSizeEnum, DeliveryProviderEnum
+from delivery import SmallestDeliveryPriceAmongProviders, Delivery, FreeDeliveryNTimesMonth, DeliveryMaxDiscountPerMonth
 from delivery_rule import DeliveryRule
 
 
@@ -16,9 +16,10 @@ class DeliveryManagerProvider:
     def get_manager(self) -> Delivery:
         
         delivery_manager = Delivery(self.delivery_rules)
-        
         if self.transaction.package_size == PackageSizeEnum.S:
             delivery_manager = SmallestDeliveryPriceAmongProviders(self.delivery_rules)
+        elif self.transaction.package_size == PackageSizeEnum.L and self.transaction.provider == DeliveryProviderEnum.LP:
+            delivery_manager = FreeDeliveryNTimesMonth(self.delivery_rules)
         
         return DeliveryMaxDiscountPerMonth(delivery_manager)
     
