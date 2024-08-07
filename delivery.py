1d38198bbab7656928fa3c96de0861c2425cfcdb
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-08-07 20:30:11 +0300

    delivery class refactor

--- delivery.py
+++ delivery.py
@@ -5,21 +5,22 @@ from transaction import Transaction
 
 class Delivery:
     
-    _delivery_price = 0
-    _discount = 0
+
+    
     
     def __init__(self, delivery_rules:list[DeliveryRule]) -> None:
+
         self.delivery_rules = delivery_rules
-    
+        self._delivery_price = 0
+        self._discount = 0
     
     def calculate(self,transaction:Transaction, member:Member) -> DeliveryData:
-        self._calculate(transaction, member)
         
-        # delivery discount manager functionality here ??
+        self._calculate(transaction, member)
         
         delivery_data = DeliveryData()
         delivery_data.delivery_price = self._delivery_price
-        # member.add_transaction(transaction=MemberTransaction(**transaction.__dict__, price=self._delivery_price))
+
         
         return delivery_data
         
@@ -27,7 +28,6 @@ class Delivery:
     def _calculate(self,transaction:Transaction, member:Member):
                 
         for rule in self.delivery_rules:
-            # if rule.provider == transaction.provider and rule.size == transaction.package_size:
             if rule.check_rule(transaction, member):
                 self._delivery_price = rule.price
                 break
